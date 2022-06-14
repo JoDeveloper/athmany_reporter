18b9e60c084c58bab76e495e3d44e173874b63da
joseph <yousuf.omer@alqimma.com.sa>
2022-06-14 13:31:29 +0300

    wip

--- lib/core/athmany_catcher.dart
+++ lib/core/athmany_catcher.dart
@@ -15,8 +15,8 @@ import 'package:sqflite/sqlite_api.dart';
 
 import 'db_service.dart';
 
-// const _uri = "http://athmany.tech/api/method/business_layer.pos_business_layer.doctype.pos_error_log.pos_error_log.new_pos_error_log";
-const _uri = "https://athmany.requestcatcher.com/test";
+const _uri = "http://athmany.tech/api/method/business_layer.pos_business_layer.doctype.pos_error_log.pos_error_log.new_pos_error_log";
+// const _uri = "https://athmany.requestcatcher.com/test";
 
 class AthmanyCatcher with ReportModeAction {
   static late AthmanyCatcher _instance;
